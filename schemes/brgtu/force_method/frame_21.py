19435d0d778163f12450d543576aaca263c8a32d
motolyanets <andrey.motolyanets@mail.ru>
2026-07-15 16:18:12 +0300

    add schema 21 to force method

--- schemes/brgtu/force_method/frame_21.py
+++ schemes/brgtu/force_method/frame_21.py
@@ -16,7 +16,7 @@ def create_frame_21(params: dict):
     i3 = params["i3"]
 
     node1 = Node(name='A', x=0, y=0)
-    node2 = Node(name='2', x=0, y=h1 * 0.5)
+    node2 = Node(name='K', x=0, y=h1 * 0.5)
     node3 = Node(name='3', x=0, y=h1)
     node4 = Node(name='4', x=l2, y=h1 + h2)
     node5 = Node(name='E', x=l2, y=h1 * 0.5, is_hinge=True)
@@ -30,6 +30,7 @@ def create_frame_21(params: dict):
     node13 = Node(name='C', x=l1 + l2, y=0)
     node14 = Node(name='D', x=l1 + l2 * 2, y=0)
     node15 = Node(name='15', x=l1 + l2 * 2, y=h1 * 0.5)
+    node16 = Node(name='16', x=l1 * 0.5 + l2, y=h1 * 0.5)
 
     rod1_1 = Rod(start_node=node1, end_node=node2)
     rod1_2 = Rod(start_node=node2, end_node=node3)
@@ -38,12 +39,15 @@ def create_frame_21(params: dict):
     rod4 = Rod(start_node=node5, end_node=node4)
     rod5 = Rod(start_node=node4, end_node=node7)
     rod6 = Rod(start_node=node7, end_node=node8, stiffness=i3)
-    # rod7 = Rod(start_node=node5, end_node=node12)
+    rod7_1 = Rod(start_node=node5, end_node=node16, is_start_hinge=True)
+    rod7_2 = Rod(start_node=node16, end_node=node12, is_end_hinge=True)
     rod8 = Rod(start_node=node8, end_node=node9, stiffness=i3)
     rod9 = Rod(start_node=node10, end_node=node9)
     rod10 = Rod(start_node=node12, end_node=node10)
     rod11 = Rod(start_node=node13, end_node=node12)
     rod12 = Rod(start_node=node10, end_node=node11, stiffness=i2)
+    rod13_1 = Rod(start_node=node14, end_node=node15)
+    rod13_2 = Rod(start_node=node15, end_node=node11)
 
     support1 = Support(node=node1, number_of_reactions=2, rotation=90)
     support2 = Support(node=node6, number_of_reactions=2, rotation=90)
@@ -51,31 +55,21 @@ def create_frame_21(params: dict):
     support4 = Support(node=node14, number_of_reactions=2, rotation=90)
 
     if load_index == 1:
-        # rod13 = Rod(start_node=node14, end_node=node11)
-        rod13_1 = Rod(start_node=node14, end_node=node15)
-        rod13_2 = Rod(start_node=node15, end_node=node11)
-
-
         load_P1 = Force(name='P', node=node8, value=P, rotation=270)
         load_q1 = DistributedForce(name='q', rod=rod2, value=q, rotation=270)
         load_q2 = DistributedForce(name='q', rod=rod12, value=q, rotation=270)
         loads = [load_P1, load_q1, load_q2]
-        nodes = [node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11, node12, node13, node14, node15]
-        # rods = [rod1_1, rod1_2, rod2, rod3, rod4, rod5, rod6, rod7, rod8, rod9, rod10, rod11, rod12, rod13_1, rod13_2]
-        rods = [rod1_1, rod1_2, rod2, rod3, rod4, rod5, rod6, rod8, rod9, rod10, rod11, rod12, rod13_1, rod13_2]
+        nodes = [node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11, node12, node13, node14, node15, node16]
+        rods = [rod1_1, rod1_2, rod2, rod3, rod4, rod5, rod6, rod7_1, rod7_2, rod8, rod9, rod10, rod11, rod12, rod13_1, rod13_2]
 
     else:
-        rod13_1 = Rod(start_node=node14, end_node=node15)
-        rod13_2 = Rod(start_node=node15, end_node=node11)
-
         load_P1 = Force(name='P', node=node2, value=P, rotation=0)
         load_P2 = Force(name='P', node=node15, value=P, rotation=180)
         load_q1 = DistributedForce(name='q', rod=rod6, value=q, rotation=270)
         load_q2 = DistributedForce(name='q', rod=rod8, value=q, rotation=270)
         loads = [load_P1, load_P2, load_q1, load_q2]
-        nodes = [node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11, node12, node13, node14, node15]
-        # rods = [rod1_1, rod1_2, rod2, rod3, rod4, rod5, rod6, rod7, rod8, rod9, rod10, rod11, rod12, rod13_1, rod13_2]
-        rods = [rod1_1, rod1_2, rod2, rod3, rod4, rod5, rod6, rod8, rod9, rod10, rod11, rod12, rod13_1, rod13_2]
+        nodes = [node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11, node12, node13, node14, node15, node16]
+        rods = [rod1_1, rod1_2, rod2, rod3, rod4, rod5, rod6, rod7_1, rod7_2, rod8, rod9, rod10, rod11, rod12, rod13_1, rod13_2]
 
     supports = [support1, support2, support3, support4]
 
@@ -98,7 +92,7 @@ def create_primary_system_21(params: dict):
     i3 = params["i3"]
 
     node1 = Node(name='A', x=0, y=0)
-    node2 = Node(name='2', x=0, y=h1 * 0.5)
+    node2 = Node(name='K', x=0, y=h1 * 0.5)
     node3 = Node(name='3', x=0, y=h1)
     node4 = Node(name='4', x=l2, y=h1 + h2)
     node5 = Node(name='E', x=l2, y=h1 * 0.5, is_hinge=True)
